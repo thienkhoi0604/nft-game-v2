1356f91b48ea1a98bb37b58812e899e9ba933230
HoangKhoi <thienkhoi0604@gmail.com>
2023-06-27 04:23:50 +0700

    create PageHOC and setup CreateBattle

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -4,6 +4,13 @@ const styles = {
   normalText: "font-rajdhani font-normal text-[24px] text-siteWhite",
   footerText: "font-rajdhani font-medium text-base text-white",
   infoText: "font-rajdhani font-medium text-lg text-siteViolet cursor-pointer",
+
+  // hoc page
+  hocContainer: "min-h-screen flex xl:flex-row flex-col relative",
+  hocContentBox:
+    "flex flex-1 justify-between bg-siteblack py-8 sm:px-12 px-8 flex-col",
+  hocLogo: "w-[160px] h-[52px] object-contain cursor-pointer",
+  hocBodyWrapper: "flex-1 flex justify-center flex-col xl:mt-0 my-16",
 };
 
 export default styles;
